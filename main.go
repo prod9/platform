60c06a6ba4c3c0e5d01b44a26aaaf5dc344fc51d
Chakrit Wichian <chakrit@prodigy9.co>
2023-12-10 20:44:44 +0700

    main: Adds support for -q and -qq to output shorter logs

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
+	"os"
 
 	fxcmd "fx.prodigy9.co/cmd"
 	"github.com/spf13/cobra"
@@ -13,7 +15,11 @@ var rootCmd = &cobra.Command{
 	Short: "PRODIGY9 platform swiss army knife",
 }
 
+var quiet int
+
 func init() {
+	rootCmd.PersistentFlags().CountVarP(&quiet, "quiet", "q", "less verbose logging")
+
 	rootCmd.AddCommand(
 		cmd.BootstrapCmd,
 		cmd.BuildCmd,
@@ -30,6 +36,19 @@ func init() {
 
 func main() {
 	defer log.Println("exited.")
+	if err := rootCmd.ParseFlags(os.Args); err != nil {
+		log.Fatalln(err)
+	}
+
+	if quiet >= 2 {
+		log.SetOutput(ioutil.Discard)
+	} else if quiet >= 1 {
+		log.SetFlags(0) // reduce
+	} else {
+		// useful in CI to have date and time stamps
+		log.SetFlags(log.Lshortfile + log.Ltime + log.Ldate)
+	}
+
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatalln(err)
 	}
