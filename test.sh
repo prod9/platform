55af9d7fe7cf03dc744db1fe99f8e85bb2147e43
Chakrit Wichian <chakrit@prodigy9.co>
2026-01-01 00:03:18 +0700

    Bump cue version

--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+CUE_VERSION=v0.15.3
+SMOKE_VERSION=v0.2.4
+
 set -o xtrace
 set -o errexit
 
-go run cuelang.org/go/cmd/cue@v0.12.1 eval -c --out=yaml tests.cue > tests.yml
-go run github.com/chakrit/smoke@v0.2.4 -v tests.yml "$@"
+go run cuelang.org/go/cmd/cue@${CUE_VERSION} eval -c --out=yaml tests.cue > tests.yml
+go run github.com/chakrit/smoke@${SMOKE_VERSION} -v tests.yml "$@"
